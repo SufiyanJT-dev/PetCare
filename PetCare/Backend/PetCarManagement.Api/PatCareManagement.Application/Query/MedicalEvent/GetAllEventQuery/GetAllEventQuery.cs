b9533f8de8ed0184bad5ee6656e22cf063b85ea7
Sufiyan J T <sufiyan.jt@geekywolf.local>
2025-12-02 09:53:32 +0530

    editpart edited

---
+++ PetCare/Backend/PetCarManagement.Api/PatCareManagement.Application/Query/MedicalEvent/GetAllEventQuery/GetAllEventQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetCareManagement.Application.Query.Pets.GetAllEventQuery
+{
+    public class GetAllEventQuery:IRequest<IEnumerable<Domain.Entity.MedicalEvent>>
+    {
+
+    }
+   
+}
---
+++ PetCare/Backend/PetCarManagement.Api/PatCareManagement.Application/Query/MedicalEvent/GetAllEventQuery/GetAllEventQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using PetCareManagement.Application.IRepository;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetCareManagement.Application.Query.Pets.GetAllEventQuery
+{
+    public class GetAllEventQueryHandler : IRequestHandler<GetAllEventQuery, IEnumerable<Domain.Entity.MedicalEvent>>
+    {
+        private readonly IGenericRepo<Domain.Entity.MedicalEvent> genericRepo;
+
+        public GetAllEventQueryHandler(IGenericRepo<Domain.Entity.MedicalEvent> genericRepo)
+        {
+            this.genericRepo = genericRepo;
+        }
+        public async Task<IEnumerable<Domain.Entity.MedicalEvent>> Handle(GetAllEventQuery request, CancellationToken cancellationToken)
+        {
+
+           return await genericRepo.GetAllAsync();
+        }
+    }
+}
